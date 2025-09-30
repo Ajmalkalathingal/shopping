db40db381b1644ca9fe7397da3a37259c2b999b4
Ajmalkalathingal <jennakkalathingal@gmail.com>
2025-09-30 13:58:10 +0530

    add order complte page

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// src/firebase.js
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+export default firebaseConfig;
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// ✅ Initialize Auth and export it
+export const auth = getAuth(app);
+export { app };
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
